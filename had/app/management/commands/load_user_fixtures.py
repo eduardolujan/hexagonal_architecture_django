632b6e5a496f74ad7c62e6d5874de8b89a7d81f3
Eduardo <eduardo.lujan.p@gmail.com>
2020-10-01 15:37:56 -0500

    Added: Model base and mappers

---
+++ had/app/management/commands/load_user_fixtures.py
@@ -0,0 +1,72 @@
+
+# -*- coding: utf-8 -*-
+
+import os
+import traceback
+
+import environ
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+
+USER_FIXTURES = {
+    'app': {
+        'develop': [
+            'user.json',
+        ]
+    }
+}
+
+
+class Command(BaseCommand):
+
+    help = 'Load all fixtures of {app}'.format(app='Users')
+
+    def add_arguments(self, parser):
+        parser.add_argument('-a', '--application',
+                            type=str,
+                            help='Define application to create')
+        parser.add_argument('-e', '--enviroment',
+                            type=str,
+                            help='Define enviroment to user')
+
+    def get_path(self):
+        app_path = environ.Path(__file__) - 3
+        fixtures_path = os.path.join(str(app_path), 'fixtures')
+        if not os.path.exists(fixtures_path):
+            raise Exception('Folder de fixtures no existe')
+        return fixtures_path
+
+    def handle(self, *args, **options):
+        application = options.get('application')
+        enviroment = options.get('enviroment')
+        fixtures_path = self.get_path()
+        print(fixtures_path)
+
+        if not application:
+            raise CommandError(f'Application param is empty')
+
+        if not enviroment:
+            raise CommandError(f'Environment param is empty')
+
+        fixtures = USER_FIXTURES.get(application, {}).get(enviroment, [])
+        if not fixtures:
+            raise CommandError(f'Not fixtures detected: {fixtures}')
+
+        for fixture in fixtures:
+            try:
+                initial_msg = f'Running {fixture} fixture'
+                self.stdout.write(self.style.SUCCESS(initial_msg))
+                fixture_path = f'users/{enviroment}/{fixture}'
+                fixture_path = os.path.join(fixtures_path, fixture_path)
+                print(fixtures_path)
+                if os.path.exists(fixture_path):
+                    call_command('loaddata', fixture_path)
+
+                else:
+                    error_msg = f'El fixture {fixture_path} no existe verifica'
+                    self.stdout.write(self.style.ERROR(error_msg))
+
+            except Exception as err:
+                print(f'Error in {__name__} fixture {fixture}, err:{err}')
+                traceback.print_exc()
+                raise CommandError('Message {}'.format(err))
--- had/app/models/users.py
+++ had/app/models/users.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import models
-from django.db.models import CharField, UUIDField
+from django.db import models as dbmodels
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -7,6 +7,8 @@ from django.utils.translation import gettext_lazy as _
 class AppUser(models.AbstractUser):
     """Default user for had."""
     #: First and last name do not cover name patterns around the globe
+    id = dbmodels.UUIDField(primary_key=True)
+
     class Meta:
         db_table = "app_user"
 
