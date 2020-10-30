07ce62c0f77c3640c6b865ef133ec6e6b499b43d
Eduardo <eduardo.lujan.p@gmail.com>
2020-10-30 13:35:44 -0600

    Added: Changes in users

--- src/users/infrastructure/repository/django/user_repository.py
+++ src/users/infrastructure/repository/django/user_repository.py
@@ -7,25 +7,62 @@ from src.users.domain.repository import UserRepository as AbstractUserRepository
 
 
 class UserRepository(OrmManager, AbstractUserRepository):
-
+    """
+    User Repository
+    """
     def __init__(self, model=AppUserModel, entity=AppUserEntity):
         super(UserRepository, self).__init__(model, entity)
 
     def get(self, entity):
+        """
+        Get user
+        @param entity: Get entity
+        @type entity: Dataclass Entity
+        @return: Model Instance (self.__model)
+        @rtype: Model
+        """
         model_instance = self.orm_get(**entity.as_dict())
         return model_instance
 
     def create(self, entity):
+        """
+        Create user
+        @param entity:
+        @type entity:
+        @return: Model Instance (self.__model)
+        @rtype: Model
+        """
         model_instance = self.orm_create(**entity.as_dict())
         return model_instance
 
     def update(self, entity):
+        """
+        Update user
+        @param entity:
+        @type entity:
+        @return: Model Instance (self.__model)
+        @rtype: Model
+        """
         model_instance = self.orm_create(**entity.as_dict())
         return model_instance
 
     def delete(self, entity):
+        """
+        Delete user
+        @param entity:
+        @type entity:
+        @return: Model Instance (self.__model)
+        @rtype: Model
+        """
         return self.orm_delete(**entity.as_dict())
 
     def all(self):
+        """
+        All user
+        @param entity:
+        @type entity:
+        @return: Model Instance (self.__model)
+        @rtype: Model
+        """
         return self.orm_all()
 
