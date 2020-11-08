0f0ba9705d669f2a53034adda2716fe5dc491044
Eduardo <eduardo.lujan.p@gmail.com>
2020-11-08 14:14:05 -0600

    Added: Changes in users

--- modules/persons/application/create/person_creator.py
+++ modules/persons/application/create/person_creator.py
@@ -5,9 +5,16 @@ from modules.shared.infrastructure.log import LoggerDecorator, PyLoggerService
 from modules.shared.domain.repository import UnitOfWork
 from modules.shared.domain.passwords import PasswordGenerator
 from modules.shared.domain.bus.command import Command
-from modules.users.domain.services import CreateUser as CreateUserService
+from modules.persons.domain.services.create import CreatePerson as CratePersonService
 from modules.users.domain.repository import UserRepository
-
+from modules.persons.domain.value_objects.person_values import (
+    PersonId,
+    Name,
+    LastName,
+    SecondLastName,
+    Address,
+    Phone
+)
 
 
 @LoggerDecorator(logger=PyLoggerService(file_path=__file__))
@@ -21,26 +28,44 @@ class PersonCreator:
                  password_generator: PasswordGenerator,
                  unit_of_work: UnitOfWork):
         """
-        Create User constructor
-        @param user_repository: User repository instance
-        @param password_generator: Password generator instance
-        @param unit_of_work: AbstractUnitOfWork
+
+        @param user_repository:
+        @type user_repository:
+        @param password_generator:
+        @type password_generator:
+        @param unit_of_work:
+        @type unit_of_work:
         """
         self.__repository = user_repository
         self.__password_generator = password_generator
         self.__unit_of_work = unit_of_work
 
-    def __call__(self, create_person_command: Command):
-        user_entity = CreateUserService.create_base_user(
-            id,
-            username,
-            password,
-            email,
-            self.__password_generator
+    def __call__(self, create_person_command: Command) -> None:
+        """
+        Create Person
+        @param create_person_command: Create person command
+        @type create_person_command: Command
+        @return: None
+        @rtype: None
+        """
+        person_id = PersonId(create_person_command.id)
+        name = Name(create_person_command.name)
+        last_name = LastName(create_person_command.last_name)
+        second_last_name = SecondLastName(create_person_command.second_last_name)
+        address = Address(create_person_command.address)
+        phone = Phone(create_person_command.phone)
+
+        person_entity = CratePersonService.create_person_entity(
+            person_id,
+            name,
+            last_name,
+            second_last_name,
+            address,
+            phone
         )
 
         with self.__unit_of_work as uow:
-            user_model_instance = self.__repository.create(user_entity)
+            user_model_instance = self.__repository.create(person_entity)
             uow.session.add(user_model_instance)
             uow.commit()
 
--- modules/persons/domain/services/create/__init__.py
+++ modules/persons/domain/services/create/__init__.py
@@ -1,6 +1,9 @@
+
+from .create_person import CreatePerson
 from .create_address import CreateAddress
 
 
 __all__ = (
+    'CreatePerson',
     'CreateAddress',
 )
