9b096455517839401fff10fc01eba91b836d1ca2
Eduardo <eduardo.lujan.p@gmail.com>
2020-11-08 16:55:21 -0600

    Added: Get person address and phone

--- modules/persons/application/get/__init__.py
+++ modules/persons/application/get/__init__.py
@@ -2,4 +2,8 @@ from .get_person import PersonGetter
 from .get_address import AddressGetter
 from .get_phone import PhoneGetter
 
-__all__ = ('PersonGetter', )
+__all__ = (
+    'PersonGetter',
+    'AddressGetter',
+    'PhoneGetter',
+)
